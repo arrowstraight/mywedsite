6c257a9fa1b56d18ebd14a15d4b7e3e20f392440
arrowstraight <37030740+arrowstraight@users.noreply.github.com>
2018-04-22 19:04:52 +0100

    java test

---
+++ changehtmlJava.js
@@ -0,0 +1,8 @@
+function myFunction() {
+ document.getElementById("changeText").innerHTML = "Hi There Friend";
+ document.getElementById("changeText").style.color = "red";
+ document.getElementById("changeText").style.fontSize = "xx-large";
+};
+
+
+
